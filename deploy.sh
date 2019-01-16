bd653e3a4d1876f3f398d3dce737d10a0d54de38
Haerul Muttaqin <haerul.muttaqin@gmail.com>
2019-01-16 12:48:06 +0700

    initial commits

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,23 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Genterate file statis
+hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
-# Go To Public folder
+# pindah ke direktoru publik
 cd public
-# Add changes to git.
-git add .
+# tambahkan perubahan ke Git
+git add -A
 
-# Commit changes.
+# Buat sebuah commit baru
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push atau puload ke Github
 git push origin master
 
-# Come Back up to the Project Root
+# Balik ke direktori sebelumnya
 cd ..
